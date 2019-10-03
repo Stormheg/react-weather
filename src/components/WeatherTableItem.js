553b2e83aad149aeba2612fdfc9bbb926d32fecf
Storm Heg <storm@stormheg.co>
2019-10-03 13:54:29 +0200

    Fix formatting

--- src/components/WeatherTableItem.js
+++ src/components/WeatherTableItem.js
@@ -1,36 +1,43 @@
-import React from 'react';
+import React from "react";
 import { formatRelative } from "date-fns";
 
-import arrow from '../assets/arrow.svg';
-import { kmhToBeafortScale } from '../utils/windSpeedConverter';
+import arrow from "../assets/arrow.svg";
+import { kmhToBeafortScale } from "../utils/windSpeedConverter";
 
-const WeatherTableItem = ({entry}) => {
-  const arrowStyle = { 
-    width: '36px',
+const WeatherTableItem = ({ entry }) => {
+  const arrowStyle = {
+    width: "36px",
     transform: `rotateZ(${entry.wind.deg}deg)`
-  }
+  };
 
-  const timeString = formatRelative(new Date(entry.dt*1000), new Date())
-  
-  const beafort = kmhToBeafortScale(entry.wind.speed)
+  const timeString = formatRelative(new Date(entry.dt * 1000), new Date());
+
+  const beafort = kmhToBeafortScale(entry.wind.speed);
 
   return (
     <tr>
-      <td>{ timeString }</td>
-      <td className="text-right">{ Math.round(entry.main.temp - 273.15) } &deg;</td>
-      <td className="text-right">{ entry.main.humidity }%</td>
+      <td>{timeString}</td>
+      <td className="text-right">
+        {Math.round(entry.main.temp - 273.15)} &deg;
+      </td>
+      <td className="text-right">{entry.main.humidity}%</td>
       <td>
-      <p>{ entry.weather[0].description }</p>
-      <img src={`https://openweathermap.org/img/w/${entry.weather[0].icon}.png`} alt={entry.weather[0].description} />
-
+        <p>{entry.weather[0].description}</p>
+        <img
+          src={`https://openweathermap.org/img/w/${entry.weather[0].icon}.png`}
+          alt={entry.weather[0].description}
+        />
       </td>
       <td className="text-right">
-        <p title={`${entry.wind.speed} km/h`}>{ beafort } bft</p>
-        <img src={arrow} alt={ `Arrow indicating ${entry.wind.deg} degrees`} style={arrowStyle} />
+        <p title={`${entry.wind.speed} km/h`}>{beafort} bft</p>
+        <img
+          src={arrow}
+          alt={`Arrow indicating ${entry.wind.deg} degrees`}
+          style={arrowStyle}
+        />
       </td>
     </tr>
   );
 };
 
 export default WeatherTableItem;
-
