906cb10a2e10ae73ef80aa1b27526d1d962a882f
Storm Heg <storm@stormheg.co>
2019-10-03 11:45:11 +0200

    Add weather arrow

--- src/components/WeatherTableItem.js
+++ src/components/WeatherTableItem.js
@@ -1,13 +1,22 @@
 import React from 'react';
+import arrow from '../assets/arrow.svg';
 
 const WeatherTableItem = ({entry}) => {
+  const arrowStyle = { 
+    width: '36px',
+    transform: `rotateZ(${entry.wind.deg}deg)`
+  }
+
   return (
     <tr>
       <td>{ entry.dt_txt }</td>
       <td className="text-right">{ Math.round(entry.main.temp - 273.15) } &deg;</td>
       <td className="text-right">{ entry.main.humidity }%</td>
       <td>{ entry.weather[0].description }</td>
-      <td className="text-right">{ entry.wind.speed } km/h</td>
+      <td className="text-right">
+        { entry.wind.speed } km/h <br/>
+        <img src={arrow} alt={ `Arrow indicating ${entry.wind.deg} degrees`} style={arrowStyle} />
+      </td>
     </tr>
   );
 };
