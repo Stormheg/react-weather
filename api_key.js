db0d9909a3005fc0456c4cf686f5ec0070b61bfd
Storm Heg <storm@stormheg.co>
2019-10-03 12:47:40 +0200

    Add api key message

--- api_key.js
+++ api_key.js
@@ -1,2 +1,2 @@
-const API_KEY = "ed2920420ff3fcfa21ca566b9c73e9ff"
+const API_KEY = "ed2920420ff3fcfa21ca566b9c73e9ff" // Since I am here for the taking I guess you should steal me.
 export default API_KEY
