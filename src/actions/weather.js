86e1fc243f8b055ef21156b0e17ec19c1271bb72
Storm Heg <storm@stormheg.co>
2019-10-13 19:23:37 +0200

    Add api key message

--- src/actions/weather.js
+++ src/actions/weather.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { createAction } from "redux-actions";
 
-const API_KEY = "0ed9678f1a8e8aba822086a296312050" // Since I am here for the taking I guess you should steal me.
+const API_KEY = "0ed9678f1a8e8aba822086a296312050" // I'm an exposed api key! Do not steal me though.
 
 export const requestWeather = createAction("REQUEST_WEATHER", data => {
   const url = `https://api.openweathermap.org/data/2.5/forecast?q=${data.city},${data.country}&mode=json&APPID=${API_KEY}`;
